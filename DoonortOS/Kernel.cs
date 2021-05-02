ea517d1f09b41651b6f6e33dfca539bbc5ac6375
Владислав Фоменко <Doonort3@outlook.com>
2021-05-02 10:47:54 +0300

    Добавьте файлы проекта.

---
+++ DoonortOS/Kernel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace doonortOS
+{
+
+    public class Kernel : Sys.Kernel
+
+    {
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine("Cosmos booted successfully.");
+            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("doonort os - ok\n"); Console.ResetColor();
+            Console.WriteLine("Write to output the commands: help");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("$ ");
+            var input = Console.ReadLine();
+            if (input == "help" | input == "Help")
+            {
+                Console.WriteLine("1) help\n All comands.");
+                Console.WriteLine("2) authors\n Project authors.");
+                Console.WriteLine("3) echo\n outputs its arguments via the standard output channel. In other words, it outputs what you wanted.");
+            }
+            if (input == "authors")
+            {
+                Console.WriteLine("\ndoonort3\nvk.com/shirakibaka");
+            };
+
+            if (input == "echo")
+            {
+                Console.WriteLine(Environment.CommandLine);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
+
